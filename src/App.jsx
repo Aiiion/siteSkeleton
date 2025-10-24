e9deb9ca1b894cee05341166641d76b91c8e3c95
Alex <alex.soderberg97@gmail.com>
2025-10-24 16:27:37 +0200

    adds data and changes design a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,68 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Show, createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import data from "./data.json";
+import "./App.css";
 
 function App() {
-  const title = 'Title';
+  const [developer, setDeveloper] = createSignal(false);
 
-  const [count, setCount] = createSignal(0)
-
+  const toggleDropdown = () => {
+    console.log(developer == true);
+    return (
+      <Show when={developer() == true}>
+        <div class="dropdown">
+          <p>
+            <b>I have:</b>
+          </p>
+          <ul>
+            {data.skills.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </Show>
+    );
+  };
   return (
     <>
+      <h1>{data.title}</h1>
+      <h3>{data.introduction}</h3>
+      <div class="card">
+        <button
+          onClick={() => {
+            setDeveloper((developer) => !developer);
+            // console.log(developer);
+          }}
+        >
+          Alex as a developer
+        </button>
+        <Show when={developer()}>
+          <div class="dropdown">
+            <p>
+              <b>I have:</b>
+            </p>
+            <ul>
+              {data.skills.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </Show>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
         <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
+          <img src={solidLogo} class="logo" alt="Solid logo" />
         </a>
       </div>
-      <h1>{title}</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
