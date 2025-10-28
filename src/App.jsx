0785522a577c5492953cc3d6f3aa23c3d1bb9578
Alex <alex.soderberg97@gmail.com>
2025-10-29 00:31:59 +0100

    adds projects to dropdown and clean up code a bit

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import weatherLogo from "./assets/weather/logo512.png";
 import weatherLogoWhite from "./assets/weather/white-logo512.png";
 import bulbDark from "./assets/light-play/bulb-dark.png";
 import bulbLight from "./assets/light-play/bulb-light.png";
-import lightIcon from "./assets/light-mode.svg"
-import darkIcon from "./assets/dark-mode.svg"
-import {arrowDown} from "./assets/icons.jsx";
+import lightIcon from "./assets/light-mode.svg";
+import darkIcon from "./assets/dark-mode.svg";
+import { arrowDown } from "./assets/icons.jsx";
 import data from "./data.json";
 import "./App.css";
 
@@ -18,6 +18,7 @@ function App() {
     ? "dark"
     : "light";
   const [skills, setSkills] = createSignal(false);
+  const [projects, setProjects] = createSignal(false);
   const [darkTheme, setDarkTheme] = createSignal(systemTheme == "dark");
   const cssSetProp = (prop, value) =>
     document.documentElement.style.setProperty(prop, value);
@@ -26,12 +27,10 @@ function App() {
       cssSetProp("--main-bg-img", "var(--light-bg-img");
       cssSetProp("--main-bg-color", "var(--light-bg-color");
       cssSetProp("--main-color", "var(--light-color");
-      cssSetProp("--main-btn-bg", "var(--light-btn-bg");
     } else {
       cssSetProp("--main-bg-img", "var(--dark-bg-img");
       cssSetProp("--main-bg-color", "var(--dark-bg-color");
       cssSetProp("--main-color", "var(--dark-color");
-      cssSetProp("--main-btn-bg", "var(--dark-btn-bg");
     }
   });
   return (
@@ -40,22 +39,25 @@ function App() {
         class="hover-underline top"
         onClick={() => setDarkTheme((theme) => !theme)}
       >
-        Use {darkTheme() ? "light" : "dark"} theme <img
-              src={darkTheme() ? lightIcon : darkIcon}
-              class="logo"
-              alt="theme icon"
-            />
+        Use {darkTheme() ? "light" : "dark"} theme{" "}
+        <img
+          src={darkTheme() ? lightIcon : darkIcon}
+          class="logo"
+          alt="theme icon"
+        />
       </button>
       <article id="main">
         <h1>{data.title}</h1>
         <h3>{data.introduction}</h3>
-        <article class="card pb-1">
+        <article class="card">
           <button
             class="button-main hover-underline"
             onClick={() => setSkills((s) => !s)}
             aria-expanded={skills()}
           >
-            <h3 class="pt-0">{data.skillsBtn} {arrowDown(skills())}</h3>
+            <h3 class="pt-0 slim">
+              {data.skillsBtn} {arrowDown(skills())}
+            </h3>
           </button>
           <section
             class="dropdown"
@@ -72,56 +74,75 @@ function App() {
             </ul>
           </section>
         </article>
-        <article class="card pt-1">
-          <h3>Projects</h3>
-          <a href="https://weather.alexbierhance.com" class="project" target="_blank">
-            <img
-              src={darkTheme() ? weatherLogoWhite : weatherLogo}
-              class="logo large"
-              alt="weather app logo"
-            />
-            <b class="decoration-none">weather</b>
-          </a>
-          <a href="https://light-play.alexbierhance.com" class="project" target="_blank">
-            <img
-              src={darkTheme() ? bulbLight : bulbDark}
-              class="logo large"
-              alt="light bulb logo"
-            />
-            <b class="decoration-none">light-play</b>
-            <small class="sub-text light">(desktop only)</small>
-          </a>
+        <article class="card pt-0">
+          <button
+            class="button-main hover-underline"
+            onClick={() => setProjects((p) => !p)}
+            aria-expanded={projects()}
+          >
+            <h3 class="pt-0 slim">Projects {arrowDown(projects())}</h3>
+          </button>
+          <section
+            class="dropdown"
+            classList={{ open: projects() }}
+            aria-hidden={!projects()}
+          >
+            <a
+              href="https://weather.alexbierhance.com"
+              class="project"
+              target="_blank"
+            >
+              <img
+                src={darkTheme() ? weatherLogoWhite : weatherLogo}
+                class="logo large"
+                alt="weather app logo"
+              />
+              <b class="decoration-none">weather</b>
+            </a>
+            <a
+              href="https://light-play.alexbierhance.com"
+              class="project"
+              target="_blank"
+            >
+              <img
+                src={darkTheme() ? bulbLight : bulbDark}
+                class="logo large"
+                alt="light bulb logo"
+              />
+              <b class="decoration-none">light-play</b>
+              <small class="sub-text light">(desktop only)</small>
+            </a>
+          </section>
         </article>
-        <p class="sub-text">
-          Check out my socials or email me to learn more
-        </p>
-        <Show when={data.github}>
-          <a href="https://github.com/Aiiion" target="_blank">
-            <img
-              src={darkTheme() ? ghLogoWhite : ghLogo}
-              class="logo small"
-              alt="github logo"
-            />
-          </a>
-        </Show>
+        <footer class="pt-1">
+          <p class="sub-text">Check out my socials or email me to learn more</p>
+          <Show when={data.github}>
+            <a href="https://github.com/Aiiion" target="_blank">
+              <img
+                src={darkTheme() ? ghLogoWhite : ghLogo}
+                class="logo small"
+                alt="github logo"
+              />
+            </a>
+          </Show>
 
-        <Show when={data.linkedIn}>
-          <a href="https://github.com/Aiiion" target="_blank">
-            <img
-              src={darkTheme() ? liLogoWhite : liLogo}
-              class="logo small"
-              alt="github logo"
-            />
-          </a>
-        </Show>
+          <Show when={data.linkedIn}>
+            <a href="https://github.com/Aiiion" target="_blank">
+              <img
+                src={darkTheme() ? liLogoWhite : liLogo}
+                class="logo small"
+                alt="github logo"
+              />
+            </a>
+          </Show>
 
-        <div>
-          <a href="mailto:alex@bierhance.se" target="_blank">
-            <b>{data.email}</b>
-          </a>
-        </div>
+          <div>
+            <a href="mailto:alex@bierhance.se" target="_blank">
+              <b>{data.email}</b>
+            </a>
+          </div>
+        </footer>
       </article>
-      {/* </div>   */}
     </>
   );
 }
